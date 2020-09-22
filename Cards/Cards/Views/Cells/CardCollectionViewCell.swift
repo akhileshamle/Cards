7efce15d1b5d3bda9ee823ae9d539fe3d7fb6dc2
Akhilesh Amle <amleakhilesh@gmail.com>
2020-09-23 02:33:35 +0530

    Added CardsViewController and Cell to work on

---
+++ Cards/Cards/Views/Cells/CardCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CardCollectionViewCell.swift
+//  Cards
+//
+//  Created by Akhilesh Amle on 23/09/20.
+//  Copyright © 2020 Akhilesh Amle. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+ 
+    @IBOutlet weak var lblMessage : UILabel!
+}
